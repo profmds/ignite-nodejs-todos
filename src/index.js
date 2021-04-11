b668d2685be3bbc2dcd12ccdaf7f53607f46d09e
profmds <marcelo@aachi.com.br>
2021-04-11 17:47:40 -0300

    feat: should be able to create a new todo

--- src/index.js
+++ src/index.js
@@ -51,7 +51,20 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  const { title, deadline } = request.body
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline,
+    created_at: new Date()
+  }
+
+  user.todos.push(todo)
+
+  return response.status(201).json(todo)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
